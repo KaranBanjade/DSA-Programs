c202bd48671199333c9a11f747399e5334348636
Karan Banjade <karan.banjade@gmail.com>
2021-11-28 23:11:29 +0530

    "Lecture 6"

---
+++ Lecture6.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    long nums;
+    cin>>nums;
+    int sum = 0, prod = 1;
+    while (nums!=0)
+    {
+        sum += nums%10;
+        prod *= nums%10;
+        nums/=10;
+    }
+    cout<<prod-sum;
+}
