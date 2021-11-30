90e5e7f6574619bc2c3c1fc4de30c92a17e72e5d
Karan Banjade <karan.banjade@gmail.com>
2021-11-30 21:29:46 +0530

    30 Nov

---
+++ Lecture9.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int amount = 1210;
+        switch(amount>=100){
+            case 1: 
+                cout<<"100: "<<amount/100<<endl;
+                amount%=100;
+                break;
+        }
+        switch(amount>=50){
+            case 1: 
+                cout<<"50: "<<amount/50<<endl;
+                amount%=50;
+                break;
+        }
+        switch(amount>=20){
+            case 1: 
+                cout<<"20: "<<amount/20<<endl;
+                amount%=20;
+                break;
+        }
+        cout<<"1: "<<amount<<endl;
+    return 0;
+}
