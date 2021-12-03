51d3ecc542b8e065b7bae4c9c3a72e47742276db
Karan Banjade <karan.banjade@gmail.com>
2021-12-03 22:13:36 +0530

    lecture 11

---
+++ Lecture11.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int returnOnes(int a){
+    int bit = 0;
+    while(a){
+        bit += a&1;
+        a = a>>1;
+    }
+    return bit;
+}
+int main(){
+    int a = 10,b = 16;
+    // a = returnOnes(a);
+    // b = returnOnes(b);
+    // int sumOnes =;
+    // sumOnes += ;
+    // cout<<a<<"\n"<<b;
+    cout<< returnOnes(a)+returnOnes(b);
+    return 0;
+}
